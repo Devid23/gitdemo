4b6dc576a97d975330691797e77ae8cd96d6fb53
DevidKarki <debitkarki01@gmail.com>
2022-06-23 19:49:19 -0500

    this is my first commit

---
+++ src/pack/Test1.java
@@ -0,0 +1,20 @@
+package pack;
+
+public class Test1 {
+	int marks;
+	String name;
+	int StudentId;
+
+	Test1(int marks, String name, int StudentId) {
+		this.marks = marks;
+		this.name = name;
+		this.StudentId = StudentId;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Each and Every Students from section-A failed Math class");
+
+	}
+
+}
